d969b5ba5a3d802fb9662e3c12f18186cea8c2e0
wjuniori <wjuniori.si@gmail.com>
2020-05-01 16:06:33 -0300

    feat: Fibonacci development

--- nodejs-0/src/index.js
+++ nodejs-0/src/index.js
@@ -1,10 +1,37 @@
-'use strict'
+"use strict";
 
-const fibonacci = () => null
+const arrayFibonacci = [];
+const limit = 350;
 
-const isFibonnaci = (num) => null
+const fibonacci = () => {
+  if (!arrayFibonacci.length) {
+    let value = 0;
+
+    while (value <= limit) {
+      arrayFibonacci.push(value);
+
+      if (value === 0) {
+        value += 1;
+      } else {
+        value += arrayFibonacci[arrayFibonacci.length - 2];
+      }
+    }
+    arrayFibonacci.push(value);
+  }
+  return arrayFibonacci;
+};
+
+const isFibonnaci = (num) => {
+  return arrayFibonacci.indexOf(num) !== -1;
+};
+
+// Para Teste Local:
+// console.log(fibonacci());
+// console.log(fibonacci());
+// console.log(isFibonnaci(5));
+// console.log(isFibonnaci(4));
 
 module.exports = {
-    fibonacci,
-    isFibonnaci
-}
+  fibonacci,
+  isFibonnaci,
+};
